c7b471ebb3e8aa17e5994babb1b90076fd106b20
chubo55 <83550539+chubo55@users.noreply.github.com>
2026-05-26 22:36:31 +0200

    Add parking and description fields to route data

--- routeData.js
+++ routeData.js
@@ -11,7 +11,10 @@ icon:"🏰",
 name:"Zamek Książ",
 time:"90 min",
 rating:"4.8/5",
-price:"35 zł"
+price:"35 zł",
+parking:"Tak",
+description:"Jeden z największych zamków w Polsce",
+photo:"📸 Idealny na zdjęcia"
 },
 
 {
@@ -19,7 +22,10 @@ icon:"🍽",
 name:"Karczma Polska",
 time:"45 min",
 rating:"4.7/5",
-price:"25–45 zł"
+price:"25–45 zł",
+parking:"Tak",
+description:"Regionalne jedzenie i duże porcje",
+photo:"📸 Klimatyczne wnętrze"
 },
 
 {
@@ -27,7 +33,10 @@ icon:"🌲",
 name:"Punkt widokowy Góry Sowie",
 time:"30 min",
 rating:"4.9/5",
-price:"Darmowe"
+price:"Darmowe",
+parking:"Tak",
+description:"Widoki na góry i lasy",
+photo:"📸 Zachody słońca"
 }
 
 ]
