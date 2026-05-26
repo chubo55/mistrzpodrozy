0b4a868615517d1acac5d0f5d7555bb0c17f2b27
chubo55 <83550539+chubo55@users.noreply.github.com>
2026-05-26 22:15:56 +0200

    Add route data for Wołomin-Świeradów Zdrój

--- routeData.js
+++ routeData.js
@@ -1,1 +1,31 @@
+const routes = {
 
+"Wołomin-Świeradów Zdrój":{
+
+travelTime:"5h 50m",
+
+stops:[
+
+{
+icon:"🏰",
+name:"Zamek po drodze",
+time:"60 min"
+},
+
+{
+icon:"🍽",
+name:"Dobra i niedroga restauracja",
+time:"45 min"
+},
+
+{
+icon:"🌲",
+name:"Punkt widokowy",
+time:"30 min"
+}
+
+]
+
+}
+
+};
